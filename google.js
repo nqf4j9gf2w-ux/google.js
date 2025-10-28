302b494b2b966f292af7f9aa7b7765244544f03c
nqf4j9gf2w-ux <nqf4j9gf2w@privaterelay.appleid.com>
2025-10-28 17:42:25 +0800

    Update google.js

--- google.js
+++ google.js
@@ -1,1 +1,15 @@
+// google_redirect.js
+// 将 google.cn / google.com.hk / google.com.tw 等域名重定向到 google.com
+let url = $request.url;
+let redirect = url.replace(/google\.(cn|com\.hk|com\.tw)/, "google.com");
 
+if (redirect !== url) {
+  $done({
+    response: {
+      status: 302,
+      headers: { "Location": redirect }
+    }
+  });
+} else {
+  $done({});
+}
